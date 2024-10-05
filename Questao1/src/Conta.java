c91b7eafe172774149b53037cccaabd7e527f93a
Nicksemery <nicolasrodriguesemery@gmail.com>
2024-10-05 19:01:44 -0300

    questão 1 pronta

--- Questao1/src/Conta.java
+++ Questao1/src/Conta.java
@@ -1,79 +1,59 @@
 public class Conta {
-
-    protected double depositar, sacar;
+    protected double x = 0;
     protected int id_conta;
     protected String titular;
 
-    double x = 0;
-
-    public Conta(){
+    public Conta() {
     }
 
-    public Conta(double depositar, double sacar, int id_conta, String titular){
+    public Conta(String titular, int id_conta) {
         this.titular = titular;
         this.id_conta = id_conta;
-        this.sacar = sacar;
-        this.depositar = depositar;
+        this.x = 0;
     }
 
-    public void setTitular(String titular){
+    public void setTitular(String titular) {
         this.titular = titular;
     }
 
-    public String getTitular(){
+    public String getTitular() {
         return titular;
     }
 
-    public void setDepositar(Double depositar){
-        this.depositar = depositar;
-    }
-    public double getDepositar(){
-        return depositar;
-    }
-
-    public void setSacar(Double sacar){
-        this.sacar = sacar;
-    }
-    public Double getSacar(){
-        return sacar;
-    }
-
-    public void setDepositar(int id){
+    public void setId_conta(int id) {
         this.id_conta = id;
     }
 
-    public double calcDep (){
-        return depositar + x;
-    }
-
-    public void setId_conta(int id){
-        this.id_conta = id;
-    }
-    public int getId_conta(){
+    public int getId_conta() {
         return id_conta;
     }
 
-    public void dep(Double valor){
-        this.x += this.x + valor;
+    public double getSaldo() {
+        return x;
     }
 
-    public void sac(double valor){
-        this.x -= this.x - valor;
+    public void depositar(double valor) {
+        if (valor > 0) {
+            this.x += valor;
+            System.out.println("Depósito de " + valor + " realizado com sucesso.");
+        } else {
+            System.out.println("Valor de depósito inválido.");
+        }
     }
 
-
-    public void detalhes(){
-        System.out.println("\n Bem vindo.");
-        System.out.println("Titular: "+ titular);
-        System.out.println("ID da conta: "+ id_conta);
-        System.out.println("Valor da conta atual: "+ x);
+    public void sacar(double valor) {
+        if (valor > 0 && valor <= x) {
+            this.x -= valor;
+            System.out.println("Saque de " + valor + " realizado com sucesso.");
+        } else {
+            System.out.println("Saldo insuficiente ou valor inválido.");
+        }
     }
 
-    public void Sd(){
-        System.out.println("Valor do saldo atual: " + this.x);
+    public void detalhes() {
+        System.out.println("\nBem-vindo.");
+        System.out.println("Titular: " + titular);
+        System.out.println("ID da conta: " + id_conta);
+        System.out.println("Valor da conta atual: " + x);
     }
-
-
-
-
 }
